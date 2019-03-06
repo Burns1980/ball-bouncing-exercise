1b831a5fdd4a5f54349ba85336bb8a972d96fe68
Burns1980 <47334305+Burns1980@users.noreply.github.com>
2019-03-06 13:02:56 -0700

    Starting point for bouncing balls exercise

---
+++ main.js
@@ -0,0 +1,143 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+
+//parent prototype. All Shapes will be derived from Shape as these properties
+//are common to them all.
+function Shape(x, y, velX, velY, exists) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exists = exists;
+}
+
+//Add exists to prototype property of Shape so this property can be inherited.
+//Shape.prototype.exists = true;
+
+//Test function to aid in vusualization of Ball inheriting from shape
+Shape.prototype.greet = function() {
+  console.log(`These are shapes constructor params: ${this.x}, ${this.y}, ${this.velX}, ${this.velY}`);
+}
+
+//The balls that will be bouncing on the screen
+function Ball(x, y, velX, velY, color, size, exists) {
+  Shape.call(this, x, y, velX, velY, exists);
+
+  //unique to Ball shape.
+  this.color = color;
+  this.size = size;
+}
+
+
+//This sets Shape's prototype values onto Ball's prototype values so that Ball can
+//inherit from Shape. After doing this though, Ball now has Shape's constructor.
+//This only needs to be done if Ball is inheriting methods from Shape.
+Ball.prototype = Object.create(Shape.prototype);
+
+//Setting the contructor back to the original for Ball.
+Object.defineProperty(Ball.prototype, 'constructor', {
+  value: Ball,
+  enumerable: false,
+  writable: true});
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+Ball.prototype.update = function() {
+
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' +
+        random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
+//This will remove any balls on the screen that make contact with it.
+//EvilCircle doesn't inherit any methods from Shape.
+function EvilCircle(x, y, exists) {
+  Shape.call(this, x, y, 20, 20, exists);
+
+  this.color = "white";
+  this.size = 10;
+}
+
+
+
+let balls = [];
+//count how many times the loop runs
+let counter = 0;
+
+function loop() {
+  counter++;
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 30) {
+    var size = random(10,50);
+    var ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      size
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+//Start the animation
+//loop();
